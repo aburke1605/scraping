2c39f5ac080a04aeaa760cd1ab6567723768d87a
aburke1605 <aodhanburke@hotmail.com>
2026-05-16 08:13:49 +1200

    create file if doesn't exist

--- MilfordTrack.py
+++ MilfordTrack.py
@@ -81,6 +81,7 @@ if were_in_business:
     # don't repeatedly send emails
 
     hash_key_file = pathlib.Path(__file__).parent.as_posix() + "/last_email_hash.txt"
+    pathlib.Path(hash_key_file).touch(exist_ok=True)
     hash_object = hashlib.sha256(body.encode("utf-8"))
     new_hash_key = hash_object.hexdigest()
     previous_hash_key = open(hash_key_file).readlines()
